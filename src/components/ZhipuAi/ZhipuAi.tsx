e852a12f4d48531052b47544593a6470701c2805
weweknow <bianzhixiaoyuan@163.com>
2023-10-01 16:54:26 +0800

    11

--- src/components/ZhipuAi/ZhipuAi.tsx
+++ src/components/ZhipuAi/ZhipuAi.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react";
 import zionMdapi from "zion-mdapi"
 
 interface ZhipuAiProps {
@@ -14,6 +15,7 @@ export function ZhipuAi(props: ZhipuAiProps) {
   //     api_key: "786b9e4cf8acffe30b7e83863545a845.tpCbmqDYbazovuaW"
   //   }
   // }
+  const [content, setContent] = useState("tips:");
   const config = {
     env: "H5",
     zhipuAi: {
@@ -21,25 +23,40 @@ export function ZhipuAi(props: ZhipuAiProps) {
     }
   }
   const mdapi = zionMdapi.init(config);
-  return <button onClick={
-    async () => {
-      props.globalData.content = "";
-      await mdapi.zhipuAi.chat({
-        prompt: [{
-          role: "user",
-          content: "你好"
-        }]
-      }, "sse-invoke", (res: any) => {
-        if (res.event != "add" && res.event != "finish") {
-          props.globalData.content = res.data
+  return (
+    <div style={
+      {
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }
+    }>
+      <div>{content}</div>
+      <button onClick={
+        async () => {
+          props.globalData.content = "";
+          setContent("");
+          await mdapi.zhipuAi.chat({
+            prompt: [{
+              role: "user",
+              content: "你好"
+            }]
+          }, "sse-invoke", (res: any) => {
+            if (res.event != "add" && res.event != "finish") {
+              props.globalData.content = "出错"
+              setContent("出错");
 
-        } else if (res.event == 'add') {
-          props.globalData.content += res.data;
+            } else if (res.event == 'add') {
+              props.globalData.content += res.data;
+              setContent(props.globalData.content);
+            }
+          })
+          console.log("globalData.content:", props.globalData.content);
         }
-      })
-      console.log("globalData.content:", props.globalData.content);
-    }
-
-
-  }>发送</button>;
+      }>发送</button>
+    </div>
+  );
 }
