1d00288bac1041329c3bfc2d38aa2ab902a50779
weweknow <bianzhixiaoyuan@163.com>
2023-10-08 13:39:34 +0800

    2233

--- src/components/ZhipuAi/ZhipuAi.tsx
+++ src/components/ZhipuAi/ZhipuAi.tsx
@@ -1,31 +1,63 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable no-empty-pattern */
+import { useEffect } from "react";
+import zionMdapi from "zion-mdapi"
 
-interface DisplayProps {
+interface ZhipuAiProps {
   globalData: Record<string, any>;
   setGlobalData: (data: Record<string, any>) => void;
-  value?: string;
+  input?: string;
+  token?: string;
+  api_key?: string;
+  isSend?: boolean;
 }
 
-export function ZhipuAi(props: DisplayProps) {
-  return (
-    <div>
-      <h1>{props.value || 'NO INPUT'}</h1>
-      <button onClick={
-        () => {
-          console.log(props)
-          
-          props.setGlobalData({
-            ...props.globalData,
-            content: "123456",
-          })
-        }}>{props.globalData.content||"test"}</button>
+export function ZhipuAi(props: ZhipuAiProps) {
+  const config = {
+    env: "H5",
+    zhipuAi: {
+      api_key: props?.api_key || "786b9e4cf8acffe30b7e83863545a845.tpCbmqDYbazovuaW",
+      token: props?.token || "",
+    }
+  }
+  const mdapi = zionMdapi.init(config);
+  let send_status = "空闲中";
+  useEffect(() => {
+    if (props.isSend && send_status == "空闲中") {
+      chat();
+    }
+    console.log('isSend changed to', props.isSend);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.isSend]);
 
-    </div>
-
-
-
-
-
-  );
+  // 发送聊天
+  async function chat() {
+    send_status = "发送中";
+    let output = "";
+    interface ZhipuAiResult {
+      event: string;
+      id: string;
+      data: string;
+      meta?: any;
+    }
+    await mdapi.zhipuAi.chat({
+      prompt: [{
+        role: "user",
+        content: props?.input || "你好"
+      }]
+    }, "sse-invoke", (res: ZhipuAiResult) => {
+      if (res.event != "add" && res.event != "finish") {
+        console.log("出错的数据：", res);
+      } else if (res.event == 'add') {
+        send_status = "进行";
+        output += res.data;
+        props.setGlobalData({ ...props.globalData, content: output })
+      } else {
+        send_status = "结束";
+        console.log(res)
+      }
+    })
+    console.log("output:", output);
+    send_status = "空闲中";
+  }
+  return (<></>)
 }
